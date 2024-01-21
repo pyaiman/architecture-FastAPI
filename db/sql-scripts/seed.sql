1ef871459da07f804ea03e169a71e24fb1d6d95f
pyaiman <prognewman@gmail.com>
2024-01-20 19:50:35 -0500

    adding changes and valdiation docker run

--- db/sql-scripts/seed.sql
+++ db/sql-scripts/seed.sql
@@ -1,10 +1,7 @@
--- Crear la base de datos
 CREATE DATABASE IF NOT EXISTS db_test;
 
--- Seleccionar la base de datos
 USE db_test;
 
--- Crear la tabla de productos
 CREATE TABLE IF NOT EXISTS products (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -12,9 +9,7 @@ CREATE TABLE IF NOT EXISTS products (
     description TEXT
 );
 
--- Insertar datos de prueba
 INSERT INTO products (name, price, description) VALUES
     ('Producto A', 19.99, 'Descripción del Producto A'),
     ('Producto B', 29.99, 'Descripción del Producto B'),
     ('Producto C', 39.99, 'Descripción del Producto C');
-mypassword
