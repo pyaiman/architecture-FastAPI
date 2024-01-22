ace1e8087c2103f6c39ff9e8befac8aaefb6cf45
pyaiman <prognewman@gmail.com>
2024-01-21 22:38:12 -0500

    adding configuration for mysql

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from usecase.products_usecase import ProductsUCase
 from interface import products_handler
 
 app = FastAPI()
-db_url = ""
+db_user = "myuser"
+db_password = "mypassword"
+db_host = "127.0.0.1"
+db_port = 3306
+db_name = "db_test"
+
+db_url = f"mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 products_repository = SqlAlchemyProductsRepository(db_url)
 products_use_case = ProductsUCase(products_repository)
@@ -13,4 +19,4 @@ products_handler = products_handler.ProductsHandler(app, products_repository, pr
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
